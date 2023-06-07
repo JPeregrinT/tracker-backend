6a0eb7549361c83fd62621bfddfcb8ed833d9e55
Jose Peregrin <tradugrin@gmail.com>
2023-06-07 18:31:54 +0200

    Login y register

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require ('express')
 const cors = require ('cors');
+const jwt = require ("jsonwebtoken");
 const { db } = require('./db/db')
 const {readdirSync} = require('fs')
 const app = express()
+const jwtSecret = process.env.JWT_SECRET;
 
 require('dotenv').config()
 
 const PORT = process.env.PORT
 
+const generalRouter = require('./routes');
  //middlewares   
 app.use(express.json())
 app.use(cors())
+app.use(generalRouter);
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+ readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+ //.get inicial para testeo
+ app.get("/", (req, res) => {
+    res.send({msg: "Hola"});
+});
+
+
 
 const server = () => {
     db()
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const generalRouter = express.Router();
+const loginRoutes = require("./login");
+const { jwtMiddleware, authRouter } = require("../security/jwt");
+
+generalRouter.use("/", authRouter);
+
+// Redirigir a middlewares de jwt
+generalRouter.use("/login", jwtMiddleware, loginRoutes);
+
+
+
+module.exports = generalRouter;
