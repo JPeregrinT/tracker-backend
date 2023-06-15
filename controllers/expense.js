8e055ba0ec827daa394d71a3742c2de325844ef9
Fernando Garrote De La Macorra <42215154+FernandoGarrote@users.noreply.github.com>
2023-06-15 20:43:22 +0200

    Se añade el post del income y expense

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,9 +1,9 @@
-const IncomeSchema= require("../models/IncomeModel")
+const ExpenseSchema= require("../models/ExpenseModel")
 
-exports.addIncome = async (req, res) => {
+exports.addExpense = async (req, res) => {
     const {title, amount, category, description, date} = req.body
 
-    const income = IncomeSchema({
+    const expense = ExpenseSchema({
         title,
         amount,
         category,
@@ -19,8 +19,8 @@ exports.addIncome = async (req, res) => {
         if(amount<=0 || amount === 'number'){
             return res.status(400).json({message: 'Amount must be a positive number'})
         }
-        await income.save()
-        res.status(200).json({message: 'Income Added'})
+        await expense.save()
+        res.status(200).json({message: 'Expense Added'})
     } catch (error){
         res.status(200).json({message: 'Server Error'})
     }
@@ -28,20 +28,20 @@ exports.addIncome = async (req, res) => {
     console.log(income)
 }
 
-exports.getIncome = async (req, res) =>{
+exports.getExpense = async (req, res) =>{
     try {
-        const incomes = await IncomeSchema.find().sort({createdAt: -1})
-        res.status(200).json(incomes)
+        const expense = await ExpenseSchema.find().sort({createdAt: -1})
+        res.status(200).json(expense)
     } catch (error){
         res.status(500).json({message: 'Server Error'})
     }
 }
 
-exports.deleteIncomes = async (req, res) =>{
+exports.deleteExpense = async (req, res) =>{
     const {id} = req.params;
-    IncomeSchema.findByIdAndDelete(id)
-        .then((income) =>{
-            res.satus(200).json({message: 'Income Deleted'})
+    ExpenseSchema.findByIdAndDelete(id)
+        .then((expense) =>{
+            res.satus(200).json({message: 'Expense Deleted'})
         })
         .catch((err) =>{
             res.status(500).json({message: 'Server Error'})
